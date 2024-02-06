6b552bf6c77e2d760b26fd3b48471e0e85ff6119
Neutreing <neutreing@gmail.com>
2024-02-06 15:08:48 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,5 @@ def search_replace(my_list, search, replace):
     z = my_list.copy()
     for i in z:
         if i == search:
-            y = z.index(search)
-            z[y] = replace
+            z[z.index(search)] = replace
     return z
