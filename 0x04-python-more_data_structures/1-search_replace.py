346553ab13f92729cfdffbc40ef978a5688b0151
Neutreing <neutreing@gmail.com>
2024-02-06 13:38:17 +0100

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    z = my_list.copy()
+    for i in z:
+        if i == search:
+            y = z.index(search)
+            z[y] = replace
+    return z 
