52904753c6b4069555cf357f200c69c33aa700bc
Neutreing <preciousoloye@gmail.com>
2024-03-01 10:08:50 +0100

    corrected 0-task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 
-"""This module contains the function 'add_integer', which adds 2 integers"""
+"""
+
+This module contains the function 'add_integer', which adds 2 integers.
+
+"""
 
 def add_integer(a, b=98):
-    """
-    This function adds two integers together and returns the result
-    as an integer.
-
-    Arguments:
-        a has a default value of zero while b has a default value of 98
-
-    Example:
-    >>> add_integer(4, 5)
-    9
-    >>> add_integer(2)
-    100
-
-    The variables must be either integers or floats. If either a or b
-    are floats, they are casted as integers. 
-    However if the values are neither integers or floats, an exception
-    called TypeError is raised.
-    """
+    """This function adds two integers together and returns the result as an integer.
+    Arguments: a has a default value of zero while b has a default value of 98
+    The variables must be either integers or floats. If either a or b are floats, they are casted as integers. However if the values are neither integers or floats, an exception called TypeError is raised."""
     if isinstance(a, (float, int)) == False:
         raise TypeError("a must be an integer")
 
